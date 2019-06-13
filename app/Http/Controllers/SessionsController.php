c98f735e285819e79616ef77cd01b92051d89554
Pikachu <251036551@qq.com>
2019-06-13 15:21:06 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,6 +27,12 @@ class SessionsController extends Controller
            return redirect()->back()->withInput();
        }
     }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
 }
 // if (Auth::attempt(['email' => $email, 'password' => $password])) {
 //     // 该用户存在于数据库，且邮箱和密码相符合
